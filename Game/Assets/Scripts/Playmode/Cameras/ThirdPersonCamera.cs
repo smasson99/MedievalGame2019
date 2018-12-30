c4c8bda608f7d8178813e48aa7d3b95fb8f986a2
Samuel Masson <smasson99@gmail.com>
2018-12-30 15:10:17 -0500

    [dev]Ajout de la rotation de la caméra autour du joueur ainsi que du linkage des contrôles de la manette xbox avec la classe du joueur.

---
+++ Game/Assets/Scripts/Playmode/Cameras/ThirdPersonCamera.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class ThirdPersonCamera : MonoBehaviour
+    {
+        [Tooltip("The player to follow.")] [SerializeField]
+        private Transform targetTransform;
+
+        [Tooltip("The distance to keep between the player and the camera.")] [SerializeField]
+        private float cameraOffsetFromPlayer = 5;
+
+        [Tooltip("The rotation speed for the camera.")] [SerializeField]
+        private float cameraRotationSpeed = 10f;
+
+        [Tooltip("The minimum rotation in degrees from the camera and the up of its follow target.")] [SerializeField]
+        private float minUpRotation = 90;
+        
+        [Tooltip("The minimum rotation in degrees from the camera and the up of its follow target.")] [SerializeField]
+        private float maxUpRotation = 150;
+        
+        private Vector2 movingDirection;
+
+        private void Awake()
+        {
+            VerifyComponents();
+        }
+
+        private void VerifyComponents()
+        {
+            if (targetTransform == null)
+            {
+                throw new NullReferenceException(nameof(targetTransform) + "can't be null.");
+            }
+        }
+
+        private void Update()
+        {
+            transform.LookAt(targetTransform);
+
+            Vector3 targetVector3 = targetTransform.position - transform.position;
+            float targetUpAngle = Vector3.Angle(targetVector3, targetTransform.up);
+            
+            if (targetUpAngle >= minUpRotation && targetUpAngle <= maxUpRotation)
+            {
+                transform.Translate(movingDirection * cameraRotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (targetUpAngle < minUpRotation)
+                {
+                    if (movingDirection.y >= 0)
+                    {
+                        transform.Translate(movingDirection * cameraRotationSpeed * Time.deltaTime);
+                    }
+                    else
+                    {
+                        Vector2 newDirection = new Vector2(movingDirection.x, 0);
+                        transform.Translate(newDirection * cameraRotationSpeed * Time.deltaTime);
+                    }
+                }
+                else if (targetUpAngle > maxUpRotation)
+                {
+                    if (movingDirection.y < 0)
+                    {
+                        transform.Translate(movingDirection * cameraRotationSpeed * Time.deltaTime);
+                    }
+                    else
+                    {
+                        Vector2 newDirection = new Vector2(movingDirection.x, 0);
+                        transform.Translate(newDirection * cameraRotationSpeed * Time.deltaTime);
+                    }
+                }
+            }
+        }
+
+        public void SetTargetTransform(Transform newTarget)
+        {
+            targetTransform = newTarget;
+        }
+
+        public void UpdateMovingDirection(Vector2 direction)
+        {
+            movingDirection = direction;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/Playmode/Players/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        private XboxOneControllerInput xboxOneControllerInput;
+        private ThirdPersonCamera thirdPersonCamera;
+
+        private void Awake()
+        {
+            InitializeComponents();
+            VerifyComponents();
+        }
+
+        private void OnEnable()
+        {
+            SubscribeToXBoxOneControllerInputs();
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeToXBoxOneControllerInputs();
+        }
+
+        private void InitializeComponents()
+        {
+            xboxOneControllerInput = GetComponentInChildren<XboxOneControllerInput>();
+            thirdPersonCamera = Camera.main?.GetComponent<ThirdPersonCamera>();
+        }
+
+        private void VerifyComponents()
+        {
+            if (xboxOneControllerInput == null)
+            {
+                throw new NullReferenceException(nameof(xboxOneControllerInput) + " not found!");
+            }
+            if (thirdPersonCamera == null)
+            {
+                throw new NullReferenceException(nameof(thirdPersonCamera) + " not found!");
+            }
+        }
+
+        private void SubscribeToXBoxOneControllerInputs()
+        {
+            xboxOneControllerInput.OnRightJoystickDirectionChanged += UpdateRightJoystickDirection;
+        }
+
+        private void UnSubscribeToXBoxOneControllerInputs()
+        {
+            xboxOneControllerInput.OnRightJoystickDirectionChanged -= UpdateRightJoystickDirection;
+        }
+
+        private void UpdateRightJoystickDirection(Vector2 direction)
+        {
+            thirdPersonCamera.UpdateMovingDirection(direction);
+        }
+    }
+}
